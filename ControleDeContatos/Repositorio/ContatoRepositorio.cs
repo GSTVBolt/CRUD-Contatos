4fc0cd5840fcb6a1164e21e96edfc8eb42ae6978
Gustavo Moura <gustavo_monteiro.20@hotmail.com>
2023-05-16 20:52:58 -0300

    Configurando as funcionalidades Editar & Apagar

--- ControleDeContatos/Repositorio/ContatoRepositorio.cs
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -1,5 +1,6 @@
 ﻿using ControleDeContatos.Data;
 using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,6 +14,10 @@ namespace ControleDeContatos.Repositorio
             _bancoContext = bancoContext;
         }
 
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
@@ -25,6 +30,34 @@ namespace ControleDeContatos.Repositorio
             _bancoContext.SaveChanges();
 
             return contato;
+        }
+
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na atualização do contato!");
+
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDB = ListarPorId(id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na deleção do contato!");
+
+            _bancoContext.Contatos.Remove(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return true;
         }
     }
 }
---
+++ ControleDeContatos/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ControleDeContatos.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
