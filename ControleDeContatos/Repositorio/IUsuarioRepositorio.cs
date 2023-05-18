74b2e3e26a141ec26325f090ee803d9718286523
Gustavo Moura <gustavo_monteiro.20@hotmail.com>
2023-05-18 12:12:08 -0300

    Criando cadastro de Usuarios

---
+++ ControleDeContatos/Repositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using ControleDeContatos.Models;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Repositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        List<UsuarioModel> BuscarTodos();
+        UsuarioModel BuscarPorID(int id);
+        UsuarioModel Adicionar(UsuarioModel usuario);
+
+        UsuarioModel Atualizar(UsuarioModel conusuariotato);
+        bool Apagar(int id);
+    }
+}
---
+++ ControleDeContatos/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,67 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public UsuarioRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+
+        public UsuarioModel BuscarPorID(int id)
+        {
+            return _bancoContext.Usuarios.FirstOrDefault(x => x.Id == id);
+        }
+        public List<UsuarioModel> BuscarTodos()
+        {
+            return _bancoContext.Usuarios.ToList();
+        }
+        public UsuarioModel Adicionar(UsuarioModel usuario)
+        {
+            // gravar no banco de dados
+
+            usuario.DataCadastro = DateTime.Now;
+            _bancoContext.Usuarios.Add(usuario);
+            _bancoContext.SaveChanges();
+
+            return usuario;
+        }
+
+        public UsuarioModel Atualizar(UsuarioModel usuario)
+        {
+            UsuarioModel usuarioDB  = BuscarPorID(usuario.Id);
+
+            if (usuarioDB == null) throw new System.Exception("Houve um erro na atualização do usuário!");
+
+            usuarioDB.Nome = usuario.Nome;
+            usuarioDB.Email = usuario.Email;
+            usuarioDB.Login = usuario.Login;
+            usuarioDB.Perfil = usuarioDB.Perfil;
+            usuarioDB.DataAtualizacao = DateTime.Now;
+            
+            _bancoContext.Usuarios.Update(usuarioDB);
+            _bancoContext.SaveChanges();
+
+            return usuarioDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            UsuarioModel usuarioDB = BuscarPorID(id);
+
+            if (usuarioDB == null) throw new System.Exception("Houve um erro na deleção do usuário!");
+
+            _bancoContext.Usuarios.Remove(usuarioDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
